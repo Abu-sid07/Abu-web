6cf48d8076681901dcb89541c736a213ee2c04a4
Abubacker Siddique R <abubackersiddique311@gmail.com>
2026-09-24 14:40:57 +0530

    project -pic bug

--- components/ui/Projects.tsx
+++ components/ui/Projects.tsx
@@ -50,7 +50,7 @@ const projects: Project[] = [
       "Onboarding platform designed to guide users through the Uni Hope experience with a clear, welcoming interface.",
     tags: ["Onboarding", "Web App", "Vercel"],
     category: "Web App",
-    images: ["/Uni-hope.png"],
+    images: ["/Uni_hope.png"],
     link: "https://uni-hope.vercel.app/",
   },
   {
